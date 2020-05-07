355e5514e75539f997af13fa248f0857be83a4f7
Claudia Santos <claudia.santos@infraclearsalecom.onmicrosoft.com>
2020-05-06 23:41:21 -0300

    correção bug menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,7 +18,7 @@ for (let i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function () {
         /* Toggle between adding and removing the "active" class,
         to highlight the button that controls the panel */
-        this.classList.toggle("active");
+        this.classList.toggle("activequestion");
 
         /* Toggle between hiding and showing the active panel */
         var panel = this.nextElementSibling;
