cc61e53d574ca419bd70e9d5156de9476c31aa72
Claudia Santos <claudia.santos@infraclearsalecom.onmicrosoft.com>
2020-05-06 21:50:20 -0300

    configuraçoes iniciais site

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+function myFunction() {
+    let menu = document.getElementById("menu");
+    let hamburger = document.querySelector('.containerhamburger .hamburger');
+    if (menu.className === "menu") {
+        menu.className += " active";
+        hamburger.className += " active";
+    } else {
+        menu.className = "menu";
+        hamburger.className = "hamburger"
+    }
+}
+
+window.onscroll = () => {
+    let header = document.querySelector("header");
+    if (header.className === "navbar") {
+        header.className += ' fundo_roxo';
+    }
+} 
